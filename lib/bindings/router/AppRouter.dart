6b8de7a9ce37a0f3be4cd65748eb00382def66a4
musyaffa.satrio <falco4ueki@gmail.com>
2025-09-07 22:53:12 +0700

    first init

---
+++ lib/bindings/router/AppRouter.dart
@@ -0,0 +1,28 @@
+import 'package:app_foundation/presentation/screens/home_screen.dart';
+import 'package:app_foundation/presentation/screens/secondscreen.dart';
+import 'package:app_foundation/presentation/screens/thirdscreen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          builder: (_) =>
+              MyHomePage(title: 'Home Screen', color: Colors.blueAccent),
+        );
+      case '/second':
+        return MaterialPageRoute(
+          builder: (_) =>
+              SecondScreen(title: "Second Screen", color: Colors.redAccent),
+        );
+      case '/third':
+        return MaterialPageRoute(
+          builder: (_) =>
+              ThirdScreen(title: "Thirst Screen", color: Colors.greenAccent),
+        );
+      default:
+        return null;
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:app_foundation/cubit/counter_cubit.dart';
+import 'package:app_foundation/bindings/router/AppRouter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final AppRouter _appRouter = AppRouter();
+
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        onGenerateRoute: _appRouter.onGenerateRoute,
+      ),
+    );
+  }
+}
