c99f93eeb43a3b59d65d56d3b3a62642643cdfe1
musyaffa.satrio <falco4ueki@gmail.com>
2026-05-05 12:45:30 +0700

    add examples

--- lib/features/store_menu/views/first_page.dart
+++ lib/features/store_menu/views/first_page.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
 
 class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+  FirstPage({super.key});
+
+  final Widget item = Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Row(
+      children: [Icon(Icons.person), SizedBox(width: 4), Text("something")],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    List<Widget> drawerList = [item, item, item];
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
+      appBar: AppBar(),
+      endDrawer: SafeArea(
+        child: Container(
+          width: 300,
+          color: Colors.white,
+          child: Column(children: drawerList),
+        ),
+      ),
+      drawer: SafeArea(
+        child: Container(
+          width: 300,
+          color: Colors.white,
+          child: Column(children: drawerList),
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(width: 2, color: Colors.red),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Text..."),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
